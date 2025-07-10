6c10d53aea6c2431a83ea4c4605b564a9f0071e2
Honore Ouedraogo <hr.ouedraogo@sanlam.bf>
2025-07-10 15:34:09 +0000

    adding fileserver links

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,3 @@
 export const BACKEND_BASE_URL = "http://localhost:4000/api/v1/erp/stag";
+export const DOC_BASE_URL = "localhost:8080/";
+export const FSASSIGN_BASE_URL = "http://localhost:9333/dir/assign";
